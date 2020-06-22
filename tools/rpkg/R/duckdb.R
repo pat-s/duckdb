f8607308e0d1188f9f0ea922455eee3ea8f0ea7e
pat-s <patrick.schratz@gmail.com>
2020-06-22 17:00:06 +0200

    separate Rd file for duckdb package

--- tools/rpkg/R/duckdb.R
+++ tools/rpkg/R/duckdb.R
@@ -1,5 +1,9 @@
-#' @details TBD.
-"_PACKAGE"
+#' duckdb Package
+#'
+#' FIXME
+#' @docType package
+#' @name duckdb-package
+NULL
 
 check_flag <- function(x) {
   if (is.null(x) || length(x) != 1 || is.na(x) || !is.logical(x)) {
