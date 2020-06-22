154dae63754aec057f29386901185752c0d43053
pat-s <patrick.schratz@gmail.com>
2020-06-22 17:33:18 +0200

    get rid of withr

--- tools/rpkg/tic.R
+++ tools/rpkg/tic.R
@@ -1,13 +1,9 @@
 # installs dependencies, runs R CMD check, runs covr::codecov()
-withr::with_dir("tools/rpkg",
-  do_package_checks(error_on = "error")
-)
+do_package_checks(error_on = "error")
 
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
 
-  withr::with_dir("tools/rpkg",
-    do_pkgdown()
-  )
+  do_pkgdown()
 }
