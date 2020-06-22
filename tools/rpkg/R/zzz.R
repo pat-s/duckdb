9a4da0a1a5bfe4b1f223cbb079c68b1d59463555
pat-s <patrick.schratz@gmail.com>
2020-06-22 17:06:18 +0200

    define more global vars

--- tools/rpkg/R/zzz.R
+++ tools/rpkg/R/zzz.R
@@ -2,5 +2,7 @@
 
   utils::globalVariables(c("duckdb_disconnect_R", "duckdb_prepare_R",
                            "duckdb_bind_R", "duckdb_ptr_to_str",
-                           "duckdb_connect_R"))
+                           "duckdb_connect_R", "duckdb_execute_R",
+                           "duckdb_register_R", "duckdb_unregister_R",
+                           "duckdb_release_R", "duckdb_startup_R"))
 }
